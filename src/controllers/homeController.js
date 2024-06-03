7631d11d29fea0b2f849771c8acf9e2820289e01
Tuandicodedao <91586684+NguyenDucTuan404@users.noreply.github.com>
2024-06-03 11:19:25 +0700

    Connrct DB

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,15 @@
 // homeController.js
 import express from 'express';
+import db from '../models/index';
 
-const getHomePage = (req, res) => {
-  return res.render('homepage.ejs');
+
+const getHomePage = async (req, res) => {
+  try {
+    const data = await db.User.findAll();
+    return res.render('homepage.ejs', { data: JSON.stringify(data) });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 export { getHomePage }; // Exporting the function using named export syntax
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import configViewEngine from './config/viewEngine.js'; // Ensure the correct path and add '.js' extension
 import dotenv from 'dotenv';
 import initWebRoutes from './route/web.js'; // Ensure the correct path and add '.js' extension
+import connectDB from './config/connectDB.js';
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -14,6 +15,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 configViewEngine(app);
 initWebRoutes(app);
+
+connectDB();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
