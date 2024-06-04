1ab74ba7ed4bd615a42d521cb24598a3f2167bee
Tuandicodedao <91586684+NguyenDucTuan404@users.noreply.github.com>
2024-06-04 12:04:05 +0700

    Create All DB

--- src/seeders/seeder-users.cjs
+++ src/seeders/seeder-users.cjs
@@ -4,9 +4,16 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Users', [{
-      firstName: 'John',
-      lastName: 'Smith',
-      email: 'john@example.com',
+      email: 'Teanne@gmail.com',
+      password: '12345',
+      firstName: 'Tean',
+      LastName: 'Ne',
+      address: 'Thai Nguyen',
+      phone: '0379174390',
+      gender: 1,
+      typeRole: 'ROLE',
+      keyRole: 'R1',
+
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
